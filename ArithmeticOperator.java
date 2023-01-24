a46b6b9b13de90c464a0b690eedfa5f2a7a7f3e7
Federico Urbani <urbanifederico5@gmail.com>
2023-01-24 22:18:52 +0100

    update arith

---
+++ ArithmeticOperator.java
@@ -0,0 +1,24 @@
+//arith class
+public class ArithmeticOperator {
+    //sum method
+    public int sum(int num1, int num2){
+
+        return num1+num2;
+    }
+    //sub method
+    public int sub(int num1, int num2){
+
+        return num1-num2;
+    }
+    //mul method
+    public int mul(int num1, int num2){
+
+        return num1*num2;
+    }
+    //sum method
+    public int div(int num1, int num2){
+
+        return num1/num2;
+    }
+
+}
---
+++ Tester.java
@@ -0,0 +1,17 @@
+//tester federico urbani
+public class Tester {
+
+    public static void main (String []args){
+        ArithmeticOperator a=new ArithmeticOperator();
+
+        // div=5
+        System.out.println(a.div(10,2));
+        //mul= 20
+        System.out.println(a.mul(2,10));
+        //sum=6
+        System.out.println(a.sum(3,3));
+        //sub=2
+        System.out.println(a.sub(10,8));
+    }
+
+}
